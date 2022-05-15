e1841acd5cb8e37efd399bea35e4af5fe476a34b
kenyansa <kenyansajustine@gmail.com>
2022-05-15 23:00:55 +0300

    understoood the passing of the props bewteen components

--- src/Parent.js
+++ src/Parent.js
@@ -4,12 +4,21 @@ import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  const [color, setColor] = useState(randomColor); //initial color state
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+
+  function handleChangeColor(newChildColor){
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor); //Updates color state to a new value
+    setChildrenColor(newChildColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      {/* state changing function passed as a prop to both Child components 
+      to allow color change when either child is clicked*/}
+      <Child onChangeColor={handleChangeColor} color={childrenColor} />
+      <Child onChangeColor={handleChangeColor} color={childrenColor} />
     </div>
   );
 }
